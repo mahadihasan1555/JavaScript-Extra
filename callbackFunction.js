5a4812b91d5d5009dd537ef74b24b1126b545b8d
mahadihasan1555 <mahadihasan1555@gmail.com>
2020-08-20 20:55:56 +0600

    callback function

---
+++ callbackFunction.js
@@ -0,0 +1,34 @@
+//when and how to use javascript callback function
+
+//A function passed to another function is often referred to as a “callback” in JavaScript.
+
+function mapAction(item) {
+    // do stuff to an item
+  }
+  array.map(mapAction)
+
+  array.map(mapAction)
+
+
+
+  function first(){
+    console.log(1);
+  }
+  function second(){
+    console.log(2);
+  }
+  first();
+  second();
+  
+//Callback function
+
+  function greeting(name) {
+    alert('Hello ' + name);
+  }
+  
+  function processUserInput(callback) {
+    var name = prompt('Please enter your name.');
+    callback(name);
+  }
+  
+  processUserInput(greeting);
